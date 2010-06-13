9fdb9a98e2d85244284d6d1b035ca354fb69d992
Toni Tuominen <toni@cloudmods.net>
2010-06-13 13:18:34 +0300

    Setting and getting options works.

--- lib/checkout_fi/payment.rb
+++ lib/checkout_fi/payment.rb
@@ -1,5 +1,50 @@
 module Checkoutfi
   class Payment
+    VALID_OPTIONS = %w[ stamp amount reference message language merchant
+                        return cancel reject delayed country currency devise content
+                        type algorithm delivery_date firstname familyname address
+                        postcode postoffice password
+                      ].map { |m| m.to_sym }.freeze
 
+    DEFAULT_OPTIONS = {
+                        :version => '0001'
+                      }
+
+    def initialize(opts = {})
+      invalid_keys = opts.keys - VALID_OPTIONS
+      if invalid_keys.size > 0
+        raise ArgumentError.new('Invalid keys: #{invalid_keys.join(", ")}')
+      end
+
+      @options = DEFAULT_OPTIONS.merge(opts)
+    end
+
+    def version
+      options[:version]
+    end
+
+    private
+
+    def options
+      @options
+    end
+
+    def method_missing(method, *args)
+      # check if we are setting or getting, setters end in = which is 61
+      setter = method.to_s[-1] == 61
+      if setter
+        method = method.to_s[0...-1].to_sym
+      end
+
+      if !VALID_OPTIONS.include?(method)
+        super
+      else
+        if setter
+          options[method] = args.first
+        else
+          options[method]
+        end
+      end
+    end
   end
 end
--- spec/payment_spec.rb
+++ spec/payment_spec.rb
@@ -1,7 +1,43 @@
 require 'spec_helper'
 
 describe 'Payment' do
+
   it 'can be instantiated' do
     doing { Checkoutfi::Payment.new }.should_not raise_error
+  end
+
+  before do
+    @payment = Checkoutfi::Payment.new
+  end
+
+  describe "#version" do
+    it 'defaults to 0001' do
+      @payment.version.should == '0001'
+    end
+
+    it 'cannot be set' do
+      doing { @payment.version = 'foo' }.should raise_error(NoMethodError)
+    end
+
+    it 'cannot be set by the constructor' do
+      doing { Checkoutfi::Payment.new(:version => '1000') }.should raise_error(ArgumentError)
+    end
+  end
+
+  describe "valid options" do
+    it 'can be set' do
+      doing { @payment.stamp = '12345' }.should_not raise_error(NoMethodError)
+    end
+
+    it 'can be read' do
+      @payment.stamp = '12345'
+      @payment.stamp.should == '12345'
+    end
+  end
+
+  describe "invalid options" do
+    it 'raises a no method error' do
+      doing { @payment.foobar = '1245' }.should raise_error(NoMethodError)
+    end
   end
 end
